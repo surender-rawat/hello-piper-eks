3de2723e52c61674b5b702d35e05ce226958a03c
Surender Rawat <surenderrawat@197NODMB27919.local>
2021-04-24 22:16:22 +0530

    nginx path change

--- src/main/java/com/paytm/hellopiper/HelloPiperApplication.java
+++ src/main/java/com/paytm/hellopiper/HelloPiperApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloPiperApplication {
 
     public static void main(String[] args) {
+       System.setProperty("server.servlet.context-path", "/data-service");
         SpringApplication.run(HelloPiperApplication.class, args);
     }
 
